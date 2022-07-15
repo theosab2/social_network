e38b1c17e85154409b3f813380f076ba7dc8e112
Théo Sabri <theo27400@live.fr>
2022-07-15 11:04:31 +0200

    Authentification

--- blogr-nextjs-prisma/pages/_app.tsx
+++ blogr-nextjs-prisma/pages/_app.tsx
@@ -1,4 +1,19 @@
-import { AppProps } from "next/app";
+// _app.tsx
+
+import { SessionProvider } from 'next-auth/react';
+import { AppProps } from 'next/app';
+
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
+};
+
+export default App;
+
+/*import { AppProps } from "next/app";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
@@ -6,4 +21,4 @@ const App = ({ Component, pageProps }: AppProps) => {
   );
 };
 
-export default App;
+export default App;*/
